8facc175c4919d76309693d6a608e7b95b72831c
Akshay H <h.akshay@outlook.com>
2018-10-25 15:13:05 +0530

    Add project files.

---
+++ DemoGenGitPolling/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web.Mvc;
+
+namespace DemoGenGitPolling.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            string contact = System.IO.File.ReadAllText(Server.MapPath("~") + @"\Template\Contact.json");
+            if (contact != "")
+            {
+                JObject obj = new JObject();
+                obj = JsonConvert.DeserializeObject<JObject>(contact);
+                string name = obj["Name"].ToString();
+                string company = obj["Company"].ToString();
+                ViewBag.Name = name;
+                ViewBag.Company = company;
+            }
+            return View();
+        }
+    }
+}
---
+++ DemoGenGitPolling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoGenGitPolling.MvcApplication" Language="C#" %>
